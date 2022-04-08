aa673ec2a260e3e02f43caa2816c1a7a34dae688
yangzhongke <yzk365@qq.com>
2022-04-08 21:29:23 +0800

    date.js finished

--- scripts/date.js
+++ scripts/date.js
@@ -47,38 +47,65 @@ Blockly.defineBlocksWithJsonArray([
 Blockly.defineBlocksWithJsonArray([
   {
     "type": "create_datetime_from",
-    "message0": "create datetime from %1-%2-%3 %4:%5:%6",
+    "message0": "create datetime %1-%2-%3 %4:%5:%6",
     "args0": [
       {
         "type": "input_value",
         "name": "YEAR",
-		"check": "NUM"
+		"check": "Number"
       },
       {
         "type": "input_value",
         "name": "MONTH",
-		"check": "NUM"
+		"check": "Number"
       },
       {
         "type": "input_value",
         "name": "DAY",
-		"check": "NUM"
+		"check": "Number"
       },
       {
         "type": "input_value",
         "name": "HOUR",
-		"check": "NUM"
+		"check": "Number"
       },
       {
         "type": "input_value",
         "name": "MINUTE",
-		"check": "NUM"
+		"check": "Number"
       },
       {
         "type": "input_value",
         "name": "SECOND",
-		"check": "NUM"
+		"check": "Number"
       },	  
+    ],
+	"inputsInline": true,
+	"output":"DateTime",
+    "style": "text_blocks"
+  }
+]);
+
+Blockly.defineBlocksWithJsonArray([
+  {
+    "type": "create_date_from",
+    "message0": "create date %1-%2-%3",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "YEAR",
+		"check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "MONTH",
+		"check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "DAY",
+		"check": "Number"
+      }	  
     ],
 	"inputsInline": true,
 	"output":"DateTime",
@@ -105,11 +132,13 @@ Blockly.JavaScript['get_datetime_part'] = function(block) {
   var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
       Blockly.JavaScript.ORDER_MODULUS);
   let code;
+  let order=Blockly.JavaScript.ORDER_FUNCTION_CALL;
   switch(type)
   {
 	  case "year":
 		code = argument0+'.'+"getFullYear()";break;
 	  case "month":
+	    order=Blockly.JavaScript.ORDER_ADDITION;
 	    code = argument0+'.'+"getMonth()+1";break;
 	  case "day":
 	    code = argument0+'.'+"getDate()";break;
@@ -122,7 +151,7 @@ Blockly.JavaScript['get_datetime_part'] = function(block) {
 	  default:
 		throw "unkown type:"+type;
   }
-  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+  return [code, order];
 };
 Blockly.CSharp['get_datetime_part'] = function(block) {
   let type = block.getFieldValue('TYPE');
@@ -157,24 +186,54 @@ Blockly.CSharp['get_datetime_part'] = function(block) {
 
 Blockly.JavaScript['create_datetime_from'] = function(block) {	
   var year = Blockly.JavaScript.valueToCode(block, 'YEAR',
-      Blockly.JavaScript.ORDER_MODULUS);
+      Blockly.JavaScript.ORDER_COMMA);
   var month = Blockly.JavaScript.valueToCode(block, 'MONTH',
-      Blockly.JavaScript.ORDER_MODULUS);
+      Blockly.JavaScript.ORDER_COMMA);
   var day = Blockly.JavaScript.valueToCode(block, 'DAY',
-      Blockly.JavaScript.ORDER_MODULUS);
+      Blockly.JavaScript.ORDER_COMMA);
   var hour = Blockly.JavaScript.valueToCode(block, 'HOUR',
-      Blockly.JavaScript.ORDER_MODULUS);
+      Blockly.JavaScript.ORDER_COMMA);
   var minute = Blockly.JavaScript.valueToCode(block, 'MINUTE',
-      Blockly.JavaScript.ORDER_MODULUS);
+      Blockly.JavaScript.ORDER_COMMA);
   var second = Blockly.JavaScript.valueToCode(block, 'SECOND',
-      Blockly.JavaScript.ORDER_MODULUS);  
+      Blockly.JavaScript.ORDER_COMMA);  
   
-  return ['new Date('+year+','+month+','+day+','+hour+','+minute+','+'second'+')', Blockly.JavaScript.ORDER_FUNCTION_CALL];
+  return ['new Date('+year+',('+month+'-1),'+day+','+hour+','+minute+','+second+')', Blockly.JavaScript.ORDER_NEW];
 };
 Blockly.CSharp['create_datetime_from'] = function(block) {
-  let type = block.getFieldValue('TYPE');
-  var argument0 = Blockly.CSharp.valueToCode(block, 'VALUE',
-      Blockly.CSharp.ORDER_MODULUS);
+  var year = Blockly.CSharp.valueToCode(block, 'YEAR',
+      Blockly.CSharp.ORDER_COMMA);
+  var month = Blockly.CSharp.valueToCode(block, 'MONTH',
+      Blockly.CSharp.ORDER_COMMA);
+  var day = Blockly.CSharp.valueToCode(block, 'DAY',
+      Blockly.CSharp.ORDER_COMMA);
+  var hour = Blockly.CSharp.valueToCode(block, 'HOUR',
+      Blockly.CSharp.ORDER_COMMA);
+  var minute = Blockly.CSharp.valueToCode(block, 'MINUTE',
+      Blockly.CSharp.ORDER_COMMA);
+  var second = Blockly.CSharp.valueToCode(block, 'SECOND',
+      Blockly.CSharp.ORDER_COMMA);  
   
-  return [argument0+'.'+funcName, Blockly.CSharp.ORDER_ATOMIC];
+  return ['new DateTime('+year+','+month+','+day+','+hour+','+minute+','+second+')', Blockly.CSharp.ORDER_NEW];
+};
+
+Blockly.JavaScript['create_date_from'] = function(block) {	
+  var year = Blockly.JavaScript.valueToCode(block, 'YEAR',
+      Blockly.JavaScript.ORDER_COMMA);
+  var month = Blockly.JavaScript.valueToCode(block, 'MONTH',
+      Blockly.JavaScript.ORDER_COMMA);
+  var day = Blockly.JavaScript.valueToCode(block, 'DAY',
+      Blockly.JavaScript.ORDER_COMMA);
+  
+  return ['new Date('+year+',('+month+'-1),'+day+')', Blockly.JavaScript.ORDER_NEW];
+};
+Blockly.CSharp['create_date_from'] = function(block) {
+  var year = Blockly.CSharp.valueToCode(block, 'YEAR',
+      Blockly.CSharp.ORDER_COMMA);
+  var month = Blockly.CSharp.valueToCode(block, 'MONTH',
+      Blockly.CSharp.ORDER_COMMA);
+  var day = Blockly.CSharp.valueToCode(block, 'DAY',
+      Blockly.CSharp.ORDER_COMMA); 
+  
+  return ['new DateTime('+year+','+month+','+day+')', Blockly.CSharp.ORDER_NEW];
 };
