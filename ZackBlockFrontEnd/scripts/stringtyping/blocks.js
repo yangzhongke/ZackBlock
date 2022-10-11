f0266f353f991c42877c5aacf16ac3a3df879eef
yangzhongke <yzk365@qq.com>
2022-10-11 16:28:23 +1300

    basic def type block

---
+++ ZackBlockFrontEnd/scripts/stringtyping/blocks.js
@@ -0,0 +1,34 @@
+Blockly.defineBlocksWithJsonArray([
+  {
+    "type": "DefVarType",
+    "message0": "Define %1 as %2",
+    "args0": [
+      {
+		'type': 'field_variable',
+		'name': 'VAR',
+		'variable': '%{BKY_VARIABLES_DEFAULT_NAME}',
+      },
+      {
+        "type": "field_dropdown",
+        "name": "TYPE",
+        "options": [
+          ["int", "int"],
+          ["string", "string"],
+          ["bool", "bool"],
+          ["double", "double"],
+          ["long", "long"],
+		  ["DateTime", "DateTime"]
+        ]
+      }	  
+    ],
+	"output":"TypeDef",
+    "style": "text_blocks",
+  }
+]);
+
+Blockly.JavaScript['DefVarType'] = function(block) {
+  return ["", Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.CSharp['DefVarType'] = function(block) {
+  return ["", Blockly.CSharp.ORDER_NONE];
+};
